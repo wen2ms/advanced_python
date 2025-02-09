f259cf1791c13a5ad3eb3a47a97cf60f3dae2e5a
wen2ms <1819509111@qq.com>
2025-02-09 16:19:46 +0800

    Feat: unpacking operator

---
+++ unpacking_operator.py
@@ -0,0 +1,17 @@
+numbers = range(10)
+
+print(*numbers)
+
+position1 = (10, 10)
+position2 = (12, 15)
+
+distance = abs(complex(*position1) - complex(*position2))
+print(distance)
+
+student_record = [10001, 34, 56, 78, 64, 23, 67, 88]
+
+student_id, *_, student_mid_score, student_final_score = student_record
+print(student_id, student_mid_score, student_final_score)
+
+print_config = {'sep': ', ', 'end': '#'}
+print(1, 2, 3, **print_config)
