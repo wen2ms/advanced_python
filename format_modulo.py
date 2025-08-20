bbdf436e4ace01bd852e57cc181c942474c395c3
wen2ms <1819509111@qq.com>
2025-08-20 15:49:43 +0800

    Feat: Formatting using modulo operator

---
+++ format_modulo.py
@@ -0,0 +1,10 @@
+word = "Hello %s"
+print(word % "python")
+
+print("name = %s, age = %d" % ("juicy", 32))
+
+print("float number = %.2f" % 3.1415926)
+
+print("Table %-10s" % "name")
+
+print("name = %(name)s, gender = %(gender)s" % {"name": "Joy", "gender": "Male"})
